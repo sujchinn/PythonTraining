f3472147a5139b273c950cdcafd89c9450755dce
sujchinn <sujchinn@cisco.com>
2015-07-15 11:12:57 +0530

    Initial Commit

---
+++ MyPackage/snakeBirth.py
@@ -0,0 +1,11 @@
+__author__ = 'sujchinn'
+
+#write a program to add two numbers given from the keyboard and display the formatted result
+a=raw_input("Enter a Value : ")
+b=raw_input("Enter b Value : ")
+c=int(a)+int(b)
+print "The sum of {0} and {1} is {2}".format(a,b,c)
+print "The sum of "+a+" and "+b+" is "+str(c)
+print "The sum of "+a+" and "+b+" is "+repr(c)
+print "The sum of",a,"and",b," is",c            #no space required after and before constant string
+
