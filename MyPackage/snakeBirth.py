b1c4e8ef5881127f5bb812fa1d5ce2a5d80a48bc
Sujeeth Kumar <sujchinn@cisco.com>
2015-07-15 11:25:55 +0530

    Removing space char

--- MyPackage/snakeBirth.py
+++ MyPackage/snakeBirth.py
@@ -5,6 +5,7 @@ a=raw_input("Enter a Value : ")
 b=raw_input("Enter b Value : ")
 c=int(a)+int(b)
 print "The sum of {0} and {1} is {2}".format(a,b,c)
+print "The sum of %s and %s is %d" %(a,b,c)
 print "The sum of "+a+" and "+b+" is "+str(c)
 print "The sum of "+a+" and "+b+" is "+repr(c)
 print "The sum of",a,"and",b,"is",c            #no space required after and before constant string
